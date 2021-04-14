0c0ab9417e11387577f5c611ba635237a1a3853d
Steve Chester <schester44@gmail.com>
2021-04-14 18:01:09 -0400

    clean up

--- src/client/components/dashboard/Layout/index.tsx
+++ src/client/components/dashboard/Layout/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { signIn, useSession } from "next-auth/client";
 import { useEffect } from "react";
 import SideNav from "../SideNav";
-import { Project, ProjectUsers, useGetCurrentUserQuery } from "@client/graphql/types.generated";
+import { ProjectUsers, useGetCurrentUserQuery } from "@client/graphql/types.generated";
 import ActionBar from "../ActionBar";
-import { ModalProvider } from "react-modal-hook";
 
 type LayoutProps = { children: React.ReactNode };
 
@@ -30,19 +29,17 @@ const Layout = ({ children }: LayoutProps) => {
   );
 
   return (
-    <ModalProvider>
-      <div className="w-full h-full flex">
-        <SideNav
-          activeProject={activeProject?.project}
-          projects={data.currentUser.projects as ProjectUsers[]}
-        />
-        <div className="flex-1">
-          <ActionBar user={data.currentUser} />
+    <div className="w-full h-full flex">
+      <SideNav
+        activeProject={activeProject?.project}
+        projects={data.currentUser.projects as ProjectUsers[]}
+      />
+      <div className="flex-1">
+        <ActionBar user={data.currentUser} />
 
-          <div className="p-4">{children}</div>
-        </div>
+        <div className="p-4">{children}</div>
       </div>
-    </ModalProvider>
+    </div>
   );
 };
 
